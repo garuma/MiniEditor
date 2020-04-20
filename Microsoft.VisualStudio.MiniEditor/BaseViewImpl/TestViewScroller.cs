2b81742329259ee63dee1445475660f87d44af47
Jérémie Laval <jeremie.laval@gmail.com>
2020-04-20 10:13:05 -0400

    Add dummy IViewScroller implementation
    While the interface doesn't make sense for MiniEditor it's still being used as a dependency in things like IEditorOperations thus implement a no-op version for those to work.

---
+++ Microsoft.VisualStudio.MiniEditor/BaseViewImpl/TestViewScroller.cs
@@ -0,0 +1,37 @@
+﻿namespace Microsoft.VisualStudio.Text.Editor.Implementation
+{
+	internal class TestViewScroller : IViewScroller
+	{
+		public static readonly TestViewScroller Instance = new TestViewScroller();
+
+		public void EnsureSpanVisible(SnapshotSpan span)
+		{
+		}
+
+		public void EnsureSpanVisible(SnapshotSpan span, EnsureSpanVisibleOptions options)
+		{
+		}
+
+		public void EnsureSpanVisible(VirtualSnapshotSpan span, EnsureSpanVisibleOptions options)
+		{
+		}
+
+		public void ScrollViewportHorizontallyByPixels(double distanceToScroll)
+		{
+		}
+
+		public void ScrollViewportVerticallyByLine(ScrollDirection direction)
+		{
+		}
+
+		public void ScrollViewportVerticallyByLines(ScrollDirection direction, int count)
+		{
+		}
+
+		public bool ScrollViewportVerticallyByPage(ScrollDirection direction) => true;
+
+		public void ScrollViewportVerticallyByPixels(double distanceToScroll)
+		{
+		}
+	}
+}
